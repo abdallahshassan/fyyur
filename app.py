56c271621557dc18d7c61b916300d976493e8ab6
Abdallah Hassan <abdallahhassan1024@gmail.com>
2020-08-19 09:23:13 +0200

    handle artists search

--- app.py
+++ app.py
@@ -77,6 +77,12 @@ class Artist(db.Model):
     # relationships
     shows = db.relationship('Show', backref="artist", lazy=True)
 
+    def get_past_shows(self):
+        return list(filter(lambda show: show.start_time <= datetime.now(), self.shows))
+
+    def get_upcoming_shows(self):
+        return list(filter(lambda show: show.start_time > datetime.now(), self.shows))
+
     def __repr__(self):
         return f'<Artist id: {self.id}, name: {self.name}, city: {self.city}, state: {self.state}>'
 
@@ -311,18 +317,28 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term', '')
+
+    # case insensitive search
+    artists = Artist.query.filter(
+        Artist.name.ilike('%' + search_term + '%')).all()
+
+    # prepare count and data of response
+    count = len(artists)
+    data = []
+    for artist in artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": len(artist.get_upcoming_shows())
+        })
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": count,
+        "data": data
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
